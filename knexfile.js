812bd15bd8cab1be1afcdaa18c7e30cb7e8ad6a3
djolekesa <djordje@deversity.com>
2024-05-15 16:49:09 +0200

    Use env variable

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection:
-      'postgres://uct5suljplp0pd:pcf6aae8fe031db5378337c8b2d5dc4afb8447b5fda543e5692cfac6230dc4e2a@cav8p52l9arddb.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d7j63psj4jftss',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
@@ -14,8 +13,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection:
-      'postgres://uct5suljplp0pd:pcf6aae8fe031db5378337c8b2d5dc4afb8447b5fda543e5692cfac6230dc4e2a@cav8p52l9arddb.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d7j63psj4jftss',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
