f2cce014a563da8c8dc226b3b6ba4c0e4143d75f
djolekesa <djordje@deversity.com>
2024-05-15 18:28:43 +0200

    Add router

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import router from './routes/routes';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
-
+app.use('/api', router);
 app.get('/', (req, res) => {
   res.json({ message: 'Hey are you alive?' });
 });
